21862e7e6975e424fd4e157b87ad8bc686df1c2f
Sveta Konstantinova <svetikons@mail.ru>
2025-03-25 20:38:03 +0300

    fix

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import ReactDOM from 'react-dom/client';
 import './styles/reset.css';
 import './styles/index.scss';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import MainPage from './pages/MainPage';
 import { store } from './redux/store';
@@ -11,17 +10,20 @@ import BooksPage from './pages/BooksPage';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
-const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <MainPage />,
-        children: [
-            { path: 'books', element: <BooksPage /> },
-            { path: 'books/:id', element: <BookPage /> },
-            { path: 'create-product', element: <CreateBook /> },
-        ],
-    },
-]);
+const router = createBrowserRouter(
+    [
+        {
+            path: '/',
+            element: <MainPage />,
+            children: [
+                { path: 'books', element: <BooksPage /> },
+                { path: 'books/:id', element: <BookPage /> },
+                { path: 'create-product', element: <CreateBook /> },
+            ],
+        },
+    ],
+    { basename: '/alfa' }
+);
 
 root.render(
     <Provider store={store}>
